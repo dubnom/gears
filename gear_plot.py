1e3c2c5f9514dee7e968471cad886a810faabafa
Glenn Gribble <git@gribbelus.com>
2020-11-22 13:38:23 -0500

    Plot the pressure line

--- gear_plot.py
+++ gear_plot.py
@@ -56,7 +56,8 @@ class Involute(object):
 
 
 class Gear(object):
-    def __init__(self, teeth=30, center=(0.0, 0.0), rot=0.0, module=1.0, pressure_angle=20.0):
+    def __init__(self, teeth=30, center=(0.0, 0.0), rot=0.0, module=1.0,
+                 pressure_angle=20.0, pressure_line=True):
         """
             Plot a gear
             :param teeth:	Number of teeth in gear
@@ -64,6 +65,7 @@ class Gear(object):
             :param rot: 	Rotation in #teeth
             :param module:	Module of gear
             :param pressure_angle: Pressure angle
+            :param pressure_line: True to plot pressure lines
         """
         self.teeth = teeth
         self.module = module
@@ -71,6 +73,7 @@ class Gear(object):
         self.pitch = self.module * pi
         self.rot = rot * self.pitch  # Now rotation is in pitch distance
         self.pressure_angle = radians(pressure_angle)
+        self.pressure_line = pressure_line
         self.pitch_radius = self.module * self.teeth / 2
         self.base_radius = self.pitch_radius * cos(self.pressure_angle)
         print('pr=%8.6f br=%8.6f cpa=%9.7f' % (self.pitch_radius, self.base_radius, cos(self.pressure_angle)))
@@ -119,14 +122,22 @@ class Gear(object):
         dedendum = self.module * 1.25
         pitch_radius = self.module * self.teeth / 2
 
+        if self.pressure_line:
+            dx = self.module*5*sin(self.pressure_angle)
+            dy = self.module*5*cos(self.pressure_angle)
+            cx = pitch_radius + self.center[0]
+            cy = 0 + self.center[1]
+            plot([(cx+dx, cy+dy), (cx-dx, cy-dy)], color='purple')
+            plot([(cx-dx, cy+dy), (cx+dx, cy-dy)], color='purple')
+
         plot(circle(pitch_radius, c=self.center), color='green')
         plot(circle(pitch_radius + addendum, c=self.center), color='yellow')
         plot(circle(pitch_radius - dedendum, c=self.center), color='blue')
         plot(circle(pitch_radius - addendum, c=self.center), color='cyan')
         plot(circle(self.base_radius, c=self.center), color='orange')
 
-        plot(circle(2 * self.module, c=self.center), color='red')
-        plot(circle(self.module, c=self.center), color='blue')
+        # plot(circle(2 * self.module, c=self.center), color='red')
+        # plot(circle(self.module, c=self.center), color='blue')
 
         plot(self.gen_poly(), color=color)
 
@@ -140,8 +151,8 @@ def do_gears(rot=0., zoom_radius=0.):
     Gear(t1, rot=rot, module=1).plot()
     print()
     # gear(30)
-    t2 = 215
-    Gear(t2, center=((t1 + t2) / 2, 0), rot=-rot).plot()
+    t2 = 5
+    Gear(t2, center=((t1 + t2) / 2, 0), rot=-rot, pressure_line=False).plot()
     plt.axis('equal')
     plt.grid()
     # Set zoom_radius to zoom in around where gears meet
@@ -152,10 +163,16 @@ def do_gears(rot=0., zoom_radius=0.):
     plt.show()
 
 
-# for rot in (n/20 for n in range(21)):
-for rot in [0, 0.125, 0.25, 0.375, 0.5][:1]:
-    do_gears(rot=rot, zoom_radius=5)
-
-if not True:
+def main():
+    # for rot in (n/20 for n in range(21)):
     for rot in [0, 0.125, 0.25, 0.375, 0.5]:
-        do_gears(rot=rot)
+        do_gears(rot=rot, zoom_radius=5)
+
+    show_unzoomed = False
+    if show_unzoomed:
+        for rot in [0, 0.125, 0.25, 0.375, 0.5]:
+            do_gears(rot=rot)
+
+
+if __name__ == '__main__':
+    main()

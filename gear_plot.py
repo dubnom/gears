2fa7fb8e19437338fdd1fc52cfd610264bf28cfa
Glenn Gribble <git@gribbelus.com>
2020-11-30 16:24:07 -0500

    First version of cycloid generating code.  No change to involute code.

--- gear_plot.py
+++ gear_plot.py
@@ -1,15 +1,32 @@
 import os
 from numbers import Number
-from typing import List, Tuple
+from typing import List, Tuple, Union
 import matplotlib.pyplot as plt
 from math import *
 
-from anim.geom import Point, Vector, Line
+from anim.geom import Point, Vector, Line, BasePoint
 from anim.transform import Transform
 from rack import Rack
 
 # setenv SHOW_INTERACTIVE to 1 or true to display interactive plots
 SHOW_INTERACTIVE = os.environ.get('SHOW_INTERACTIVE', 'false').lower() in {'1', 'true'}
+
+
+def t_range(steps, t_low=0.0, t_high=1.0, closed=True):
+    """Range from t_low to t_high in steps.  If closed, include t_high"""
+    t_len = t_high - t_low
+    return (step / steps * t_len + t_low for step in range(0, steps + int(closed)))
+
+
+def path_rotate(path, angle, as_pt=False):
+    """Rotate all points by angle (in degrees) about 0,0"""
+    t = Transform().rotate(angle)
+    return [t.transform_pt(pt, as_pt) for pt in path]
+
+
+def path_translate(path, dx, dy, as_pt=False):
+    """Rotate all points by angle (in degrees) about 0,0"""
+    return [(x+dx, y+dy) for x, y in path]
 
 
 def circle(r, c=(0, 0)):
@@ -68,7 +85,7 @@ class Gear(object):
                  module=1.0, relief_factor=1.25,
                  pressure_angle=20.0, pressure_line=True):
         """
-            Plot a gear
+            Plot an involute gear
             :param teeth:	Number of teeth in gear
             :param center:  Center of gear
             :param rot: 	Rotation in #teeth
@@ -367,29 +384,265 @@ class Gear(object):
         plt.show()
 
 
-def do_gears(rot=0., zoom_radius=0.):
+class GearBase:
+    def __init__(self, module, teeth):
+        self.module = module
+        self.teeth = teeth
+        self.circular_pitch = self.module * pi
+        self.pitch_diameter = self.module * self.teeth
+        self.pitch_radius = self.pitch_diameter / 2
+
+    def gen_poly(self):
+        """Generate the polygon of the gear at (0,0) with 0 rotation"""
+        ...
+
+    def plot(self, zoom_radius=0.0):
+        """Plot the gear poly and associated construction lines/circles"""
+        ...
+
+    def plot_show(self, zoom_radius=0.0):
+        plt.axis('equal')
+        plt.grid()
+        # Set zoom_radius to zoom in around where gears meet
+        if zoom_radius:
+            ax = plt.gca()
+            ax.set_xlim(self.teeth / 2 - zoom_radius, self.teeth / 2 + zoom_radius)
+            ax.set_ylim(-zoom_radius, zoom_radius)
+        plt.show()
+
+
+class GearCycloidal(GearBase):
+    def __init__(self,
+                 module=1.0, wheel_teeth=30, pinion_teeth=8,        # Definitional attributes
+                 generating_radius=0.0,                             # Defaults to pinion_radius/2
+                 ):
+        """
+            Plot a cycloidal gear
+            :param teeth:	Number of teeth in gear
+            :param module:	Module of gear
+            :param relief_factor: extra for dedendum
+        """
+        super().__init__(module, wheel_teeth)
+        self.wheel_teeth = wheel_teeth
+        self.pinion_teeth = pinion_teeth
+        self.wheel_pitch_radius = self.module * self.wheel_teeth / 2
+        self.pinion_pitch_radius = self.module * self.pinion_teeth / 2
+        self.wheel_tooth_degrees = 360 / self.wheel_teeth
+        self.pinion_tooth_degrees = 360 / self.pinion_teeth
+
+        self.generating_radius = generating_radius or self.pinion_pitch_radius / 2
+
+        self.pitch = self.module * pi
+        aft, theta = self.calc_addendum_factor()
+        self.addendum_factor_theoretical = aft
+        self.addendum_factor = 0.95 * self.addendum_factor_theoretical
+        self.wheel_tooth_theta = theta
+        self.addendum = self.addendum_factor * self.module
+        self.dedendum = self.addendum_factor_theoretical * 1.05 * self.module
+        self.wheel_base_radius = self.wheel_pitch_radius - self.dedendum
+        self.pinion_base_radius = self.pinion_pitch_radius - (self.addendum_factor + 0.4) * self.module
+        self.tip_radius = self.pitch_radius + self.addendum
+        # print('pr=%8.6f af=%8.6f' % (self.pitch_radius, self.addendum_factor))
+
+    def calc_cycloid(self, theta) -> Point:
+        # https://en.wikipedia.org/wiki/Epicycloid
+        rr = self.generating_radius + self.wheel_pitch_radius
+        return Point(
+            rr * cos(theta) - self.generating_radius * cos(rr / self.generating_radius * theta),
+            rr * sin(theta) - self.generating_radius * sin(rr / self.generating_radius * theta)
+        )
+
+    def cycloid_path(self, theta_min=0.0, theta_max=pi/2, steps=5) -> List[BasePoint]:
+        vals = ['%.4f' % theta for theta in t_range(steps, theta_min, theta_max)]
+        # print('cp: tm=%.4f %s' % (theta_max, ', '.join(vals)))
+        return [self.calc_cycloid(theta) for theta in t_range(steps, theta_min, theta_max)]
+
+    def calc_addendum_factor(self) -> Tuple[float, float]:
+        """Calculate addendum height and corresponding theta"""
+        # Equations from https://www.csparks.com/watchmaking/CycloidalGears/index.jxl
+
+        pr_gr = self.wheel_pitch_radius / self.generating_radius
+
+        def theta_func(beta):
+            return pi / self.pinion_teeth + pr_gr * beta
+
+        def beta_func(theta):
+            return atan2(sin(theta), (1 + pr_gr - cos(theta)))
+
+        theta_guess = 1.0
+        beta_guess = 0.0
+        error = 1.0
+        max_error = 1e-10
+        while error > max_error:
+            beta_guess = beta_func(theta_guess)
+            theta_new = theta_func(beta_guess)
+            error = abs(theta_new - theta_guess)
+            theta_guess = theta_new
+        k = 1 + pr_gr
+        af = self.pinion_teeth/4 * (1 - k + sqrt(1 + k*k - 2*k*cos(theta_guess)))
+        debug_stuff = False
+        if debug_stuff:
+            print('calc_af: af=%.4f beta=%.4f theta=%.4f' % (af, beta_guess, theta_guess))
+            ht = tau / self.wheel_teeth / 4
+            pt = self.calc_cycloid(ht)
+            print('   : ht=%.4f cc=%s ccl=%.4f' % (ht, pt.round(4), (pt-Point(0, 0)).length()))
+            print('   : ')
+        return af, theta_guess / pr_gr
+
+    def gen_poly(self, rotation=0.0) -> List[Tuple[Number, Number]]:
+        rotation *= self.wheel_tooth_degrees
+        half_tooth = self.wheel_tooth_degrees / 4
+        cycloid_path_down = list(reversed(self.cycloid_path(theta_max=self.wheel_tooth_theta, steps=30)))
+        cycloid_path_up = [Point(p.x, -p.y) for p in reversed(cycloid_path_down)]
+
+        tooth_path = []
+        tip_high = path_rotate(cycloid_path_up, - half_tooth, True)
+        tip_low = path_rotate(cycloid_path_down, half_tooth, True)
+        origin = Point(0, 0)
+
+        def scale_pt(pt: Point):
+            return (pt-origin).unit()*self.wheel_base_radius + origin
+
+        tooth_path.append(scale_pt(tip_high[0]))
+        tooth_path.extend(tip_high)
+        tooth_path.extend(tip_low)
+        tooth_path.append(scale_pt(tip_low[-1]))
+
+        points = []
+        # for n in [-1, 0, 1]:
+        for n in range(self.teeth):
+            mid = n * self.wheel_tooth_degrees + rotation
+            points.extend(path_rotate(tooth_path, mid))
+        points.append(points[0])
+
+        return points
+
+    def gen_pinion_poly(self, rotation=0.0, center=(0, 0)) -> List[Tuple[Number, Number]]:
+        if self.pinion_teeth % 2 == 0:
+            rotation += 0.5
+        rotation *= self.pinion_tooth_degrees
+        half_tooth = tau / self.pinion_teeth / 4
+        if self.pinion_teeth < 10:
+            half_tooth *= 1.05 / (pi/2)
+            if self.pinion_teeth < 8:
+                a, b, c, d = [fa*0.855/1.05 for fa in [0.25, 0.55, 0.8, 1.05]]
+            else:
+                a, b, c, d = [fa*0.670/1.05 for fa in [0.25, 0.55, 0.8, 1.05]]
+        else:
+            half_tooth *= 1.25 / (pi/2)
+            a, b, c, d = [fa*0.625/0.55 for fa in [0.2, 0.35, 0.5, 0.55]]
+        tooth_path_polar = [
+            # radius          angle
+            (self.pinion_base_radius, 2.5*half_tooth),
+            (self.pinion_base_radius, half_tooth),
+            (self.pinion_pitch_radius, half_tooth),
+            (self.pinion_pitch_radius+a*self.module, half_tooth*0.9),
+            (self.pinion_pitch_radius+b*self.module, half_tooth*0.7),
+            (self.pinion_pitch_radius+c*self.module, half_tooth*0.4),
+            (self.pinion_pitch_radius+d*self.module, 0),
+            (self.pinion_pitch_radius+c*self.module, -half_tooth*0.4),
+            (self.pinion_pitch_radius+b*self.module, -half_tooth*0.7),
+            (self.pinion_pitch_radius+a*self.module, -half_tooth*0.9),
+            (self.pinion_pitch_radius, -half_tooth),
+            (self.pinion_base_radius, -half_tooth),
+            (self.pinion_base_radius, -2.5*half_tooth),
+        ]
+        tooth_path_polar = tooth_path_polar
+        tooth_path = [Point(r*cos(t), r*sin(t)) for r, t in tooth_path_polar]
+
+        points = []
+        # for n in [-1, 0, 1]:
+        for n in range(self.teeth):
+            mid = n * self.pinion_tooth_degrees + rotation
+            points.extend(path_translate(path_rotate(tooth_path, mid), *center))
+
+        # points.append(points[0])
+
+        return points
+
+    def plot(self, color='blue', rotation=0.0, center=Point(0, 0), pinion: Union[str, bool] = True):
+        addendum = self.module * self.addendum_factor_theoretical
+        dedendum = self.pitch / 2
+        pitch_radius = self.pitch_radius
+
+        if pinion != 'only':
+            plot(circle(pitch_radius, c=center), color='green')
+            plot(circle(pitch_radius + addendum, c=center), color='yellow')
+            plot(circle(pitch_radius - dedendum, c=center), color='cyan')
+            # plot(circle(pitch_radius - addendum, c=center), color='cyan')
+            plot(self.gen_poly(rotation=rotation), color=color)
+
+        if pinion:
+            pinion_pitch = self.pinion_pitch_radius
+            p_center = (pitch_radius+pinion_pitch, 0)
+            plot(circle(pinion_pitch, c=p_center), color='green')
+            plot(circle(pinion_pitch + addendum, c=p_center), color='yellow')
+            plot(circle(self.pinion_base_radius, c=p_center), color='cyan')
+            plot(self.gen_pinion_poly(rotation=-rotation, center=p_center), color=color)
+
+        plt.title(r'$\sum_{idiot}^{\infty}sin(m^a_n)$')
+
+
+def do_pinions(zoom_radius=0., cycloidal=True):
+    wheel = None
+    rot = 0.5
+    color_index = 0
+    colors = ['blue', 'red', 'green', 'orange']
+    for pt in [6, 8, 10, 12]: #range(5, 11):
+        # print(circle(2))
+        # plot(circle(1, (1, -.5)), color='blue')
+        # rot = 0.25
+        # rot = 0.0
+        t1 = 40
+        t2 = pt
+        # Gear(t1, rot=rot, module=1).plot(mill_space=True)
+        if cycloidal:
+            wheel = GearCycloidal(1, t1, t2)
+            pinion = None
+        else:
+            wheel = Gear(t1, rot=rot, module=1)
+            pinion = Gear(t2, center=((t1 + t2) / 2, 0), rot=-rot, pressure_line=False)
+
+        wheel.plot(colors[color_index % len(colors)], rotation=rot, pinion='only')
+        color_index += 1
+        if pinion:
+            pinion.plot('green')
+    wheel.plot_show(zoom_radius)
+
+
+def do_gears(rot=0., zoom_radius=0., cycloidal=True, pt=6):
     # print(circle(2))
     # plot(circle(1, (1, -.5)), color='blue')
     # rot = 0.25
     # rot = 0.0
-    t1 = 19
+    t1 = 40
+    t2 = pt
     # Gear(t1, rot=rot, module=1).plot(mill_space=True)
-    Gear(t1, rot=rot, module=1).plot()
-    print()
-    # gear(30)
-    t2 = 5
-    # Gear(t2, center=((t1 + t2) / 2, 0), rot=-rot, pressure_line=False).plot()
-    plt.axis('equal')
-    plt.grid()
-    # Set zoom_radius to zoom in around where gears meet
-    if zoom_radius:
-        ax = plt.gca()
-        ax.set_xlim(t1 / 2 - zoom_radius, t1 / 2 + zoom_radius)
-        ax.set_ylim(-zoom_radius, zoom_radius)
-    plt.show()
+    if cycloidal:
+        wheel = GearCycloidal(1, t1, t2)
+        pinion = None
+    else:
+        wheel = Gear(t1, rot=rot, module=1)
+        pinion = Gear(t2, center=((t1 + t2) / 2, 0), rot=-rot, pressure_line=False)
+
+    wheel.plot('blue', rotation=rot)
+    if pinion:
+        pinion.plot('green')
+    wheel.plot_show(zoom_radius)
 
 
 def main():
+    do_pinions(zoom_radius=5); return
+    for pt in [5, 6, 7, 8, 9, 10, 11]:
+        do_gears(zoom_radius=5, pt=pt)
+    return
+
+    for rot in [0, 0.125, 0.25, 0.375, 0.5]:
+        do_gears(rot=rot, zoom_radius=5)
+    return
+
+    do_gears(); return
+
     radii = [8, 4, 2, 1]
     radii = [8, 4]
     radii = [2]
@@ -398,9 +651,6 @@ def main():
     return
 
     # for rot in (n/20 for n in range(21)):
-    for rot in [0, 0.125, 0.25, 0.375, 0.5]:
-        do_gears(rot=rot, zoom_radius=5)
-
     show_unzoomed = False
     if show_unzoomed:
         for rot in [0, 0.125, 0.25, 0.375, 0.5]:

5f50b5a94acba90e4f538b3b886fa77da516dc8e
Michael Dubno <michael@dubno.com>
2020-12-09 15:46:27 -0500

    Added a max module size to planets, and a preference for the largest module that fits the parameters.

--- planets.py
+++ planets.py
@@ -14,6 +14,7 @@ planets = [
 # Module = diameter / teeth
 totalDiameter = 6 * 25.4
 minModule = .7
+maxModule = 1.3
 
 print("{:10} {:>3} {:>3} {:>8}  {:>6} {:>6} {:>6} {:>6} {:>6}".format('Planet', 'x', 'y','mod', 'err','x Diam', 'y Diam', 'x Rad', 'y rad'), '\n')
 for planet in planets:
@@ -23,9 +24,9 @@ for planet in planets:
         for x in range(20, 200):
             ratio = x/y
             error = abs((planet[1] - ratio) / planet[1])
-            if error <= prevError:
+            if error < prevError:
                 module = totalDiameter / (x + y)
-                if module < minModule:
+                if module < minModule or module > maxModule:
                     continue
                 prevError = error
                 best = (x,y,module,round(100*error,2), (x+2)*module, (y+2)*module, (x+2)*module/2, (y+2)*module/2)
